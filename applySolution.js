6e6c1c8ea9aadd4679997a6043e9499ccfa57d11
sebastiangilpatino <sebastiangilpatino@gmail.com>
2021-04-28 16:36:37 -0500

    "wrapping function for call/apply JS"

---
+++ applySolution.js
@@ -0,0 +1,12 @@
+let group = {
+    title: "Our Group",
+    students: ["John", "Pete", "Alice"],
+
+    showList: function () {
+        let helper = (student) => (student) => console.log(this.title + ": " + student);
+        this.students.forEach(helper.apply(this));
+    }
+
+};
+
+group.showList();
